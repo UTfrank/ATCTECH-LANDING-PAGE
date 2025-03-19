1317ad9dea8306d84c0b9fb978103494d2639c0d
UTfrank <frankjoe261@gmail.com>
2025-03-19 13:47:01 +0100

    chore: implement invisible scroll in brand companies section

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -3,7 +3,7 @@ import { partners } from '@/constants'
 
 const Brands = () => {
     return (
-        <div className='border-t border-t-[#DCDCDC] pt-6 md:pt-12 flex flex-row justify-between items-center overflow-scroll'>
+        <div className='border-t border-t-[#DCDCDC] pt-6 md:pt-12 flex flex-row justify-between items-center overflow-x-scroll scrollbar-hidden'>
             {partners.map(item => (
                 <div key={item.id}><Image src={item.image} sizes='100vw' width={500}
                     height={300} alt='partner companies' className='w-full h-auto' /></div>
